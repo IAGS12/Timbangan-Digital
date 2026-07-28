2781d4730884a26996cfa9a4bb055a23703d007f
IAGS12 <indraagustin77@gmail.com>
2026-07-28 20:51:46 +0700

    feat: seed 3 initial weighings per breed and support manual weight entry for linear regression

--- config/database.go
+++ config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"time"
 	"github.com/jmoiron/sqlx"
 	_"modernc.org/sqlite"
 )
@@ -114,27 +115,41 @@ func createTables() {
 	if cowCount == 0 {
 		seeds := []struct {
 			code, name, breed string
+			w1, w2, w3        float64
 		}{
-			{"BALI-01", "Sapi Bali", "Sapi Bali"},
-			{"MAD-01", "Sapi Madura", "Sapi Madura"},
-			{"PO-01", "Sapi PO", "Peranakan Ongole"},
-			{"LIM-01", "Sapi Limosin", "Sapi Limosin"},
-			{"SIM-01", "Sapi Simental", "Sapi Simental"},
-			{"FH-01", "Sapi FH", "Friesian Holstein"},
-			{"BRH-01", "Sapi Brahman", "Sapi Brahman"},
-			{"ACEH-01", "Sapi Aceh", "Sapi Aceh"},
-			{"PES-01", "Sapi Pesisir", "Sapi Pesisir"},
-			{"BRG-01", "Sapi Brangus", "Sapi Brangus"},
-			{"WAG-01", "Sapi Wagyu", "Sapi Wagyu"},
-			{"ANG-01", "Sapi Angus", "Black Angus"},
-			{"HER-01", "Sapi Hereford", "Sapi Hereford"},
-			{"CHA-01", "Sapi Charolais", "Sapi Charolais"},
-			{"JAB-01", "Sapi Jabres", "Sapi Jabres"},
+			{"BALI-01", "Sapi Bali", "Sapi Bali", 280.0, 310.0, 345.0},
+			{"MAD-01", "Sapi Madura", "Sapi Madura", 250.0, 275.0, 305.0},
+			{"PO-01", "Sapi PO", "Peranakan Ongole", 320.0, 350.0, 385.0},
+			{"LIM-01", "Sapi Limosin", "Sapi Limosin", 420.0, 460.0, 505.0},
+			{"SIM-01", "Sapi Simental", "Sapi Simental", 430.0, 470.0, 515.0},
+			{"FH-01", "Sapi FH", "Friesian Holstein", 380.0, 410.0, 445.0},
+			{"BRH-01", "Sapi Brahman", "Sapi Brahman", 350.0, 385.0, 425.0},
+			{"ACEH-01", "Sapi Aceh", "Sapi Aceh", 220.0, 245.0, 270.0},
+			{"PES-01", "Sapi Pesisir", "Sapi Pesisir", 190.0, 210.0, 235.0},
+			{"BRG-01", "Sapi Brangus", "Sapi Brangus", 390.0, 425.0, 465.0},
+			{"WAG-01", "Sapi Wagyu", "Sapi Wagyu", 360.0, 395.0, 435.0},
+			{"ANG-01", "Sapi Angus", "Black Angus", 400.0, 440.0, 485.0},
+			{"HER-01", "Sapi Hereford", "Sapi Hereford", 410.0, 450.0, 495.0},
+			{"CHA-01", "Sapi Charolais", "Sapi Charolais", 440.0, 485.0, 535.0},
+			{"JAB-01", "Sapi Jabres", "Sapi Jabres", 260.0, 285.0, 315.0},
 		}
+
+		t1 := time.Now().AddDate(0, -2, 0).Format("2006-01-02 15:04:05")
+		t2 := time.Now().AddDate(0, -1, 0).Format("2006-01-02 15:04:05")
+		t3 := time.Now().Format("2006-01-02 15:04:05")
+
 		for _, s := range seeds {
-			_, _ = DB.Exec(`INSERT INTO cows (cow_code, name, breed, gender, status) VALUES (?, ?, ?, 'jantan', 'active')`, s.code, s.name, s.breed)
+			res, err := DB.Exec(`INSERT INTO cows (cow_code, name, breed, gender, status) VALUES (?, ?, ?, 'jantan', 'active')`, s.code, s.name, s.breed)
+			if err == nil {
+				cowID, _ := res.LastInsertId()
+				adg1 := (s.w2 - s.w1) / 30.0
+				adg2 := (s.w3 - s.w2) / 30.0
+				_, _ = DB.Exec(`INSERT INTO weight_records (cow_id, weight, adg, measurement_date, device_id) VALUES (?, ?, ?, ?, 'SEEDER-INIT')`, cowID, s.w1, 0.0, t1)
+				_, _ = DB.Exec(`INSERT INTO weight_records (cow_id, weight, adg, measurement_date, device_id) VALUES (?, ?, ?, ?, 'SEEDER-INIT')`, cowID, s.w2, adg1, t2)
+				_, _ = DB.Exec(`INSERT INTO weight_records (cow_id, weight, adg, measurement_date, device_id) VALUES (?, ?, ?, ?, 'SEEDER-INIT')`, cowID, s.w3, adg2, t3)
+			}
 		}
-		log.Println("15 Rumpun Sapi standar berhasil di-seed ke database.")
+		log.Println("15 Rumpun Sapi standar + 3 penimbangan awal per sapi berhasil di-seed ke database.")
 	}
 
 	log.Println("Semua tabel database siap.")
